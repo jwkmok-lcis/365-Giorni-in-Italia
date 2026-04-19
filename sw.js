045cc24e55bbab64eb8f6c01588b971d55979da7
jwkmok-lcis <jwkmok@gmail.com>
2026-04-19 12:57:19 +0000

    Fix: Force service worker update with skipWaiting/clients.claim and cache bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "giorni-v7";
+const CACHE_NAME = "giorni-v8";
 const ASSETS = [
   "./",
   "./index.html",
@@ -40,10 +40,12 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
+  self.clients.claim();
   e.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
